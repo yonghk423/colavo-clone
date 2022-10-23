6bda05bfe4e9d0920ce6b4784ba9e504c6d8b3b1
yonghk423 <yonghk423423@gmail.com>
2022-10-24 03:34:26 +0900

    home style ui

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -44,20 +44,21 @@ const Home:React.FC<Props> = ( { cartItems, addToCart, removeFromCart, discountI
                     <div className='infoName'>김용희</div>
                 </div>
                 <div className='btnBox'>                    
-                    <Button onClick={surgeryClick} style={{backgroundColor: "#ede7f6"}} variant="contained">시술</Button>
-                    <Button onClick={DiscountClick} style={{backgroundColor: "#ffb2dd"}} variant="contained">할인</Button>
+                    <Button onClick={surgeryClick} style={{backgroundColor: "#ede7f6", margin: "5px"}} variant="contained">시술</Button>
+                    <Button onClick={DiscountClick} style={{backgroundColor: "#ffb2dd", margin: "5px"}} variant="contained">할인</Button>
                 </div>
             </InfoBtnBox>
             <DataBox>
                 {cartItems?.map((ele, id) => (
                     <div className='data' key={id}>
-                        <div>{ele?.name}</div>
-                        <div>{ele?.price}원</div>
-                        <div>{ele?.count}개</div>                       
+                        <div className='name'>{ele?.name}</div>
+                        <div className='price'>{ele?.price}원</div>
+                        <div className='count'>{ele?.count}개</div>                       
                         <Button
                             size='small'
                             disableElevation
                             variant='contained'
+                            style={{backgroundColor: "#d1c4e9" , width: '50px'}}
                             onClick={() => addToCart(ele)}
                         >
                             +
@@ -66,6 +67,7 @@ const Home:React.FC<Props> = ( { cartItems, addToCart, removeFromCart, discountI
                             size='small'
                             disableElevation
                             variant='contained'
+                            style={{backgroundColor: "#d1c4e9" , width: '50px'}}
                             onClick={() => removeFromCart(ele)}
                         >
                             -
@@ -74,18 +76,18 @@ const Home:React.FC<Props> = ( { cartItems, addToCart, removeFromCart, discountI
                 ))}
                 {discountOption?.map((ele, id) => (
                     <div className='data' key={id}>    
-                        <div>{ele.name}</div>
-                        <div>{ele.rate}</div>
+                        <div className='name'>{ele.name}</div>
+                        <div className='rate'>{ele.rate}%</div>
                     </div>    
                 ))}
             </DataBox>
             <FooterBox>
                 <TotalCountBox>
-                    <div>합계</div>
+                    <div className='total'>합계</div>
                     <div>{test.length === 0 ? ttest2 : test2 }원</div>
                 </TotalCountBox>
                 <NextBtn>
-                    <Button variant="contained">확인</Button>
+                    <Button style={{backgroundColor: "#d1c4e9" , width: '300px'}} variant="contained">다음</Button>
                 </NextBtn>
             </FooterBox>
         </Container>
@@ -98,7 +100,7 @@ const Container = styled.div`
     margin: 30px;
     background-color: rgba(225, 225, 225, 0.2);
     border-radius: 15px;
-    box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px     rgba(0, 0, 0, 0.06);  
+    box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);  
 `;
 
 const InfoBtnBox = styled.div`
@@ -110,6 +112,7 @@ const InfoBtnBox = styled.div`
         font-weight: bolder;
         .infoName {
             margin: 10px;
+            font-size: 20px;
         }
     }
     .btnBox {
@@ -123,7 +126,27 @@ const InfoBtnBox = styled.div`
 const DataBox = styled.div`
     height: 450px;
     .data {
+        border-top: 1px solid #9e9e9e;
         margin: 10px;
+        .name {
+            font-weight: bolder;
+            margin: 10px;
+        }
+        .price {
+            color:#9e9e9e;
+            font-size: 12px; 
+            margin: 5px;
+        }
+        .count {
+            color:#9e9e9e;
+            font-size: 12px; 
+            margin: 5px;
+        }
+        .rate {
+            color:#9e9e9e;
+            font-size: 12px; 
+            margin: 5px;
+        }
     }
     
 `;
@@ -133,6 +156,8 @@ const FooterBox = styled.div`
     justify-content: center;
     justify-items: center;
     font-weight: bolder;
+    border-top: 1px solid #9e9e9e;
+    margin: 10px;
 `;
 
 const TotalCountBox = styled.div`
@@ -140,6 +165,10 @@ const TotalCountBox = styled.div`
     display: grid;
     grid-template-columns: auto auto;
     gap: 100px;   
+    .total {
+        color:#9e9e9e;
+        font-size: 15px; 
+    }
 `;
 
 const NextBtn = styled.div`
